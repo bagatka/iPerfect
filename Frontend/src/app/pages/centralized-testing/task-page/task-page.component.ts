9f5656ca9c714cf41e074f9173a0b513c2809ae1
Alexander Bagatka <alex.bagatka@gmail.com>
2022-06-08 05:42:25 +0300

    RC

--- Frontend/src/app/pages/centralized-testing/task-page/task-page.component.ts
+++ Frontend/src/app/pages/centralized-testing/task-page/task-page.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { TaskComponentMode } from 'src/app/components/tasks/task-component-base.directive';
 import { Subject } from '../../../components/tasks/models';
 
@@ -8,7 +8,7 @@ import { Subject } from '../../../components/tasks/models';
   templateUrl: './task-page.component.html',
   styleUrls: ['./task-page.component.scss']
 })
-export class TaskPageComponent implements OnInit {
+export class TaskPageComponent {
   @Input() taskId!: string;
   @Input() taskCatalog!: string;
   @Input() taskSeed!: number;
@@ -16,12 +16,6 @@ export class TaskPageComponent implements OnInit {
 
   get TaskComponentMode(): typeof TaskComponentMode {
     return TaskComponentMode;
-  }
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
   }
 
   isImageAnswers(subject: Subject, catalog: string, id: string): string {
